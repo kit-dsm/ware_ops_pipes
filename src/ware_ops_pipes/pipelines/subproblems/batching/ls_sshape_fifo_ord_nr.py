6d30c7c5a61af419db85125ae0974dcc21105371
j4n1k <janik.bischoff@gmx.de>
2026-07-23 11:42:19 +0200

    corrected routing kwargs for LSBatchingSShapeFiFoOrderNr component

--- src/ware_ops_pipes/pipelines/subproblems/batching/ls_sshape_fifo_ord_nr.py
+++ src/ware_ops_pipes/pipelines/subproblems/batching/ls_sshape_fifo_ord_nr.py
@@ -22,15 +22,11 @@ class LSBatchingSShapeFiFoOrderNr(MultiOrderBatching):
             "distance_matrix": layout_network.distance_matrix,
             "predecessor_matrix": layout_network.predecessor_matrix,
             "picker": resources.resources,
-            "n_aisles": graph_params.n_aisles,
-            "n_pick_locations": graph_params.n_pick_locations,
-            "dist_aisle": graph_params.dist_aisle,
-            "dist_pick_locations": graph_params.dist_pick_locations,
-            "dist_aisle_location": graph_params.dist_bottom_to_pick_location,
-            "dist_start": graph_params.dist_start,
-            "dist_end": graph_params.dist_end,
             "gen_tour": False,
-            "gen_item_sequence": False
+            "gen_item_sequence": False,
+            "node_list": layout_network.node_list,
+            "node_to_idx": {node: idx for idx, node in enumerate(list(layout_network.graph.nodes))},
+            "idx_to_node": {idx: node for idx, node in enumerate(list(layout_network.graph.nodes))}
         }
 
         batcher = LocalSearchBatching(
